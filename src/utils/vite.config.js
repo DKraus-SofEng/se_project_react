01e624c29f74c02d4ced96c04737dd38ab44ffa4
DELIGHT KRAUS <dkraus.sofeng@gmail.com>
2026-01-06 20:04:59 -0700

    Bug fix

--- src/utils/vite.config.js
+++ src/utils/vite.config.js
@@ -4,11 +4,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "DKraus-SofEng/se_project_react"
+    base: "/se_project_react/",
     plugins: [react()],
     test: {
         environment: "jsdom",
         globals: true,
     },
 });
-
